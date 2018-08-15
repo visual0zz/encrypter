9a93ba54f34d1a5f266fa17d8427c3a4253e0e1f
Zhaozhi <indiancarriernoac@qq.com>
2018-08-15 23:06:57 +0800

    测试

--- src/main/java/com/zz/RandomGenerator.java
+++ src/main/java/com/zz/RandomGenerator.java
@@ -1,24 +1,44 @@
 package com.zz;
 
+import static com.zz.HashService.*;
+
 public class RandomGenerator{
 
-    State state;//储存当前的随机数状态
-    public void setSeed(String seed){ state=new State(seed);}
-    public void setSeed(int seed){state=new State(seed);}
-
+    byte[] state=null;//储存当前的随机数状态
+    public void setSeed(String seed){ state=seed.getBytes();}
+    public void setRandomSeed(){state=md5.getRandomHash().getByteArray();}
+    RandomGenerator(){setRandomSeed();bytecount=-5;}//负数迫使其第一次输出时重新计算state
+    RandomGenerator(String seed){setSeed(seed);bytecount=-5;}
+    int bytecount;//用于输出byte时计数
     public static int hash(String source){
         //todo 要替换成自己的哈希函数
         return source.hashCode();
     }
-
+    private void generateNextState(){
+        state=md5.getHash(state).getByteArray();
+    }
+    public byte getNextByte(){//获得伪随机字节
+        bytecount++;
+        if(bytecount<state.length && bytecount>=0){
+            return state[bytecount];
+        }
+        else{
+            bytecount=0;
+            generateNextState();
+            return state[bytecount];
+        }
+    }
     public int getNextInt(){
-        state.generateNextState();
-        return state.getInt();
+        return  getNextByte()+
+                getNextByte()<<8+
+                getNextByte()<<16+
+                getNextByte()<<24
+                ;
     }
 
     public char getNextChar(){
-        state.generateNextState();
-        return state.getChar();
+        return (char) (getNextByte()+
+                        getNextByte()<<8);
     }
     public char getNextPrintableChar(){
         char c;
@@ -26,20 +46,9 @@ public class RandomGenerator{
         do {
             count++;
             c=getNextChar();
-            if(count>=1000) c= (char) (c%26+'a');
+            if(count>=1000) c= 'a';
         }while(Character.isLetter(c));
         return c;
-    }
-
-    public String getNextStringWithCertainLength(int length){
-        if(length<0){
-            throw new NegativeArraySizeException("字符串长度不能小于0!");
-        }
-        StringBuilder builder=new StringBuilder();
-        for(int i=0;i<length;i++){
-            builder.append(getNextChar());
-        }
-        return builder.toString();
     }
     public String getNextPrintableStringWithCertainLength(int length){
         if(length<0){
@@ -50,28 +59,6 @@ public class RandomGenerator{
             builder.append(getNextPrintableChar());
         }
         return builder.toString();
-    }
-
-    static class State{//表示一个随机数生成器的状态
-        int value;
-        State(State state){
-            this.value =state.value;
-        }
-        State(int state){
-            value=state;
-        }
-        State(String state){
-            value=state.hashCode();//todo 要仔细设计这里
-        }
-        int getInt(){
-            return value;
-        }
-        char getChar(){
-            return (char)value;
-        }
-        private void generateNextState(){//得到下一个状态
-            value =hash(String.valueOf(value));
-        }
     }
 }
 
