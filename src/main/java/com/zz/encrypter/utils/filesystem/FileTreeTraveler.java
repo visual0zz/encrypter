ee8c5f3f2141d43bbad65404c1b11f6db2d7e186
Zhaozhi <indiancarriernoac@qq.com>
2018-08-17 22:27:56 +0800

    file tree traveler

---
+++ src/main/java/com/zz/encrypter/utils/filesystem/FileTreeTraveler.java
@@ -0,0 +1,21 @@
+package com.zz.encrypter.utils.filesystem;
+
+
+import java.io.File;
+
+public interface FileTreeTraveler {
+    /**
+     * @param layer_level 当前文件夹在迭代树上的层数
+     * @param folder 当前文件夹
+     * @param result 用于用户自定义功能的实现，迭代完成后会返回这个值，可以在travel代码中对其进行更改。
+     * @return 如果返回true会继续深入此文件夹进行迭代，返回false不会继续迭代这个文件夹下面的内容。
+     */
+    boolean travelFolder(int layer_level, File folder, String[] result);
+
+    /**
+     * @param layer_level 当前文件在迭代树上的层数
+     * @param file 当前文件
+     * @param result 用于用户自定义功能的实现，迭代完成后会返回这个值，可以在travel代码中对其进行更改。
+     */
+    void travelFile(int layer_level,File file,String[] result);
+}
