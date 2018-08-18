492f0f51218676da0b1c3714170672aeb5e7f089
Zhaozhi <indiancarriernoac@qq.com>
2018-08-18 19:01:16 +0800

    input encrypter

---
+++ src/main/java/com/zz/encrypter/utils/cryptography/InputStreamEncrypter.java
@@ -0,0 +1,55 @@
+package com.zz.encrypter.utils.cryptography;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputStreamEncrypter extends InputStream {
+    public InputStreamEncrypter() {
+        super();
+    }
+
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return super.read(b);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return super.read(b, off, len);
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        return super.skip(n);
+    }
+
+    @Override
+    public int available() throws IOException {
+        return super.available();
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+        super.mark(readlimit);
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+        super.reset();
+    }
+
+    @Override
+    public boolean markSupported() {
+        return super.markSupported();
+    }
+}
